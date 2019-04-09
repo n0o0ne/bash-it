c5eb232a91a05d83c274e9cdbce2ffda12cc2ce5
n0| <n0one.git@gmail.com>
2019-04-09 15:31:31 +0200

    Erster Test der Konsolenausgabe

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const {withHermes} = require('Hermes-Javascript')
+const {withHermes} = require('Hermes-Javascript') /* Die Hermes zu Javascript Bibliothek wird eingebunden. Sie realisiert die Weitergabe der in der STT erkannten Schlüsselworte und deren Intents */
+
+//Funktion zum ausführen von Konsolenbefehlen
+function execute(command) 
+{
+	const exec = require('child_process').exec
+
+	exec(command, (err, stdout, stderr) => 
+	{
+        	process.stdout.write(stdout)
+	})
+}
+
 
 withHermes(hermes => {
 	const dialog = hermes.dialog()
 
-	dialog.flow('n01:StartScript', (msg, flow) => {
+	dialog.flow('n01:ExecBash', (msg, flow) => /* Wenn der Intent, der von Hermes übergeben wird, StartScript ist wird eine Konsolenbefehlt ausgeführt */
+	{
 		console.log(msg)
-		
+		execute('echo "Test" > /home/n01/test.txt') /**/
 		flow.end()
-		return "Ok, wird ausgeführt"
-		})
-
+		return "Der Test Text wurde in die Testdatei geschrieben!"
 	})
+
+})
